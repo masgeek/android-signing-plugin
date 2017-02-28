ab590f9da7f663d48128b2a5e434ee37f7fd0477
restjohn <restjohn@users.noreply.github.com>
2017-02-28 11:52:58 -0700

    added job-dsl support

---
+++ src/main/java/org/jenkinsci/plugins/androidsigning/SignApksDslContext.java
@@ -0,0 +1,54 @@
+package org.jenkinsci.plugins.androidsigning;
+
+
+import hudson.Extension;
+import javaposse.jobdsl.dsl.Context;
+import javaposse.jobdsl.dsl.helpers.step.StepContext;
+import javaposse.jobdsl.plugin.ContextExtensionPoint;
+import javaposse.jobdsl.plugin.DslExtensionMethod;
+
+@Extension(optional = true)
+public class SignApksDslContext extends ContextExtensionPoint {
+
+    public static class ConfigureContext implements Context {
+
+        private final SignApksBuilder builder;
+
+        private ConfigureContext(SignApksBuilder builder) {
+            this.builder = builder;
+        }
+
+        public void keyStoreId(String x) {
+            builder.setKeyStoreId(x);
+        }
+
+        public void keyAlias(String x) {
+            builder.setKeyAlias(x);
+        }
+
+        public void archiveSignedApks(boolean x) {
+            builder.setArchiveSignedApks(x);
+        }
+
+        public void archiveUnsignedApks(boolean x) {
+            builder.setArchiveUnsignedApks(x);
+        }
+
+        public void androidHome(String x) {
+            builder.setAndroidHome(x);
+        }
+
+        public void zipalignPath(String x) {
+            builder.setZipalignPath(x);
+        }
+    }
+
+    @DslExtensionMethod(context = StepContext.class)
+    public SignApksBuilder signAndroidApks(String apksToSign, Runnable configClosure) {
+        SignApksBuilder builder = new SignApksBuilder();
+        builder.setApksToSign(apksToSign);
+        executeInContext(configClosure, new ConfigureContext(builder));
+        return builder;
+    }
+
+}
---
+++ src/test/groovy/org/jenkinsci/plugins/androidsigning/SignApksDslContextTest.groovy
@@ -0,0 +1,68 @@
+package org.jenkinsci.plugins.androidsigning
+
+import hudson.model.FreeStyleProject
+import javaposse.jobdsl.plugin.ExecuteDslScripts
+import org.junit.Rule
+import org.junit.Test
+import org.jvnet.hudson.test.JenkinsRule
+
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.nullValue
+import static org.junit.Assert.*
+
+class SignApksDslContextTest {
+
+    @Rule
+    public JenkinsRule testJenkins = new JenkinsRule();
+
+    @Test
+    void parsesSignApksDsl() {
+        ExecuteDslScripts dslScripts = new ExecuteDslScripts(
+            """
+            job('${this.class.simpleName}-generated') {
+                steps {
+                    signAndroidApks '**/*-unsigned.apk', {
+                        keyStoreId 'my.keyStore'
+                        keyAlias 'myKey'
+                        archiveSignedApks true
+                        archiveUnsignedApks true
+                        androidHome '/fake/android-sdk'
+                    }
+                    signAndroidApks '**/*-other.apk', {
+                        keyStoreId 'my.otherKeyStore'
+                        keyAlias 'myOtherKey'
+                        archiveSignedApks false
+                        archiveUnsignedApks false
+                        zipalignPath '/fake/android-sdk/zipalign'
+                    }
+                }
+            }
+            """)
+        FreeStyleProject job = testJenkins.createFreeStyleProject("${this.class.simpleName}-seed")
+        job.buildersList.add(dslScripts)
+        testJenkins.buildAndAssertSuccess(job)
+        job = testJenkins.jenkins.getItemByFullName("${this.class.simpleName}-generated", FreeStyleProject)
+
+        assertThat(job.builders.size(), equalTo(2))
+
+        SignApksBuilder signApks = job.builders[0]
+
+        assertThat(signApks.apksToSign, equalTo("**/*-unsigned.apk"))
+        assertThat(signApks.keyStoreId, equalTo("my.keyStore"))
+        assertThat(signApks.keyAlias, equalTo("myKey"))
+        assertTrue(signApks.archiveSignedApks)
+        assertTrue(signApks.archiveUnsignedApks)
+        assertThat(signApks.androidHome, equalTo("/fake/android-sdk"))
+        assertThat(signApks.zipalignPath, nullValue())
+
+        signApks = job.builders[1]
+
+        assertThat(signApks.apksToSign, equalTo("**/*-other.apk"))
+        assertThat(signApks.keyStoreId, equalTo("my.otherKeyStore"))
+        assertThat(signApks.keyAlias, equalTo("myOtherKey"))
+        assertFalse(signApks.archiveSignedApks)
+        assertFalse(signApks.archiveUnsignedApks)
+        assertThat(signApks.androidHome, nullValue())
+        assertThat(signApks.zipalignPath, equalTo("/fake/android-sdk/zipalign"))
+    }
+}
